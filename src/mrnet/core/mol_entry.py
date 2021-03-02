4dd05c82f4c7436e9f5c81f93022ecfc1b7bb15f
Shyam D <shyamd@lbl.gov>
2021-03-02 10:26:45 -0800

    fix mypy issue

--- src/mrnet/core/mol_entry.py
+++ src/mrnet/core/mol_entry.py
@@ -95,7 +95,7 @@ class MoleculeEntry(MSONable):
         """
         try:
             if isinstance(mol_doc["molecule"], Molecule):
-                molecule = mol_doc["molecule"]
+                molecule = mol_doc["molecule"]  # type : ignore
             else:
                 molecule = Molecule.from_dict(mol_doc["molecule"])
             energy = mol_doc["energy_Ha"]
@@ -170,7 +170,7 @@ class MoleculeEntry(MSONable):
             if isinstance(doc["molecule"], Molecule):
                 molecule = doc["molecule"]
             else:
-                molecule = Molecule.from_dict(doc["molecule"])
+                molecule = Molecule.from_dict(doc["molecule"])  # type: ignore
 
             if (
                 thermo == "rrho_shifted"
